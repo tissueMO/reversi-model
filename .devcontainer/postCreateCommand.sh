2a7cd084c3cbc8e28141015aa33f6f1cfa688189
tissueMO <ts32worker@gmail.com>
2025-03-29 17:45:03 +0000

    Devcontainer起動時のコマンドの可読性を向上

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,4 +2,9 @@
 
 nvidia-smi
 
-python -c 'import tensorflow as tf; print(\"TensorFlow version:\", tf.__version__); print(\"GPU Available:\", len(tf.config.list_physical_devices(\"GPU\")) > 0); print(\"GPU Devices:\", tf.config.list_physical_devices(\"GPU\"))'
+python -c '
+import tensorflow as tf
+print("TensorFlow version:", tf.__version__)
+print("GPU Available:", len(tf.config.list_physical_devices("GPU")) > 0)
+print("GPU Devices:", tf.config.list_physical_devices("GPU"))
+'
